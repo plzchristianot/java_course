b12b845de3a9ec945bbdb68ad3c762d939438b31
plzchristianot <oviedotorreschristian@gmail.com>
2026-03-14 16:27:18 -0600

    primer commit

---
+++ Chapter1/UnderstandingTheJavaClassStructure/src/Comments.java
@@ -0,0 +1,33 @@
+public class Comments {
+
+    // comment until end of line
+
+    //------------------------------------------
+    /* Multiple
+     * line comment
+     */
+
+    //-------------------------------------------
+
+    /**
+     * Javadoc multiple-line comment
+     * @author Jeanne and Scott
+     */
+
+    //---------------EJERCICiOS-----------------------
+
+    /*
+     * // anteater multi-line comment
+     */
+
+    // bear single-line comment
+
+    // // cat single-line comment
+
+    // /* dog */ single-line comment
+
+    /* elephant */ //multi-line comment
+    /*
+     * /* ferret */ //multi-line comment
+    // */
+}
---
+++ Chapter1/WritingAMainMethod/src/Zoo.java
@@ -0,0 +1,7 @@
+class Zoo {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        //If I compile this and pass in only one argument to the class I wont work and it will throw an exception
+    }
+}
