1d1b4668d1e36ef92bf19aba987069cdeb19a3ca
plzchristianot <oviedotorreschristian@gmail.com>
2026-06-06 01:39:25 -0600

    feat: se termina de subir cap2

---
+++ Chapter2/UnderstandingAdvancedFlowControl/src/TheBreakStatement/SearchSample.java
@@ -0,0 +1,24 @@
+package TheBreakStatement;
+
+public class SearchSample {
+    public static void main(String[] args){
+        int[][] list = {{1,13,5},{1,2,5},{2,7,2}};
+        int searchValue = 2;
+        int positionX = -1;
+        int positionY = -1;
+        PARENTLOOP: for(int i=0; i<list.length; i++){
+            for(int j=0; j<list[i].length; j++){
+                if(list[i][j]==searchValue){
+                    positionX = i;
+                    positionY = j;
+                    break PARENTLOOP;
+                }
+            }
+        }
+        if(positionX==-1 || positionY==-1){
+            System.out.println("Value "+searchValue+" found at: " + "("+positionX+","+positionY+")");
+        } else {
+            System.out.println("Value "+searchValue+" found at: " + "("+positionX+","+positionY+")");
+        }
+    }
+}
